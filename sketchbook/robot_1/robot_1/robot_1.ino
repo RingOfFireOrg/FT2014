0acf8d8101eef4c3f8e7f5f6849dcdf93f49b547
Kylebrown9 <slaveofschool@gmail.com>
2014-12-04 21:47:33 -0500

    What i said i did in the prior push

--- sketchbook/robot_1/robot_1/robot_1.ino
+++ sketchbook/robot_1/robot_1/robot_1.ino
@@ -12,8 +12,9 @@ Adafruit_StepperMotor *myStepper2 = AFMStop.getStepper(200, 2);
 boolean flip = false;
 boolean state1 = true, firstWiggle = true;
 long lastSwitch, switchTime = 2000;
+double interval = 10;
 
-#define STEP_TYPE SINGLE
+#define STEP_TYPE DOUBLE;
 
 void forwardstep1() {  
   myStepper1->onestep(FORWARD, SINGLE);
@@ -33,46 +34,45 @@ AccelStepper stepper1(forwardstep1, backwardstep1);
 AccelStepper stepper2(forwardstep2, backwardstep2);
 //Brysons code
 void go_forward(void){
-  moveWheels(40,40);
+  moveWheels(interval,interval);
 }
 
 void go_left(void){
-  moveWheels(0,40);
+  moveWheels(0,interval);
 }
 
 void go_right(void){
-  moveWheels(40,0);
+  moveWheels(interval,0);
 }
 
 void go_backward(void){
-  moveWheels(-40,-40);
+  moveWheels(-interval,-interval);
 }
 
 void reverse_left(void){
-  moveWheels(-40,0);
+  moveWheels(-interval,0);
 }
 
 void reverse_right(void){
-  moveWheels(0,-40);
+  moveWheels(0,-interval);
 }
 
 void moveWheels(int wheel1, int wheel2) {
   int lWheel, rWheel;
-  
+
   if(flip) {
     lWheel = wheel2;
     rWheel = wheel1;
-  } else {
+  } 
+  else {
     lWheel = wheel1;
     rWheel = wheel2;
   }
-  
+
   stepper1.move(lWheel);
   stepper2.move(rWheel);
+
   
-  //while (stepper1.distanceToGo() != 0 && stepper2.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
   //}
 }
 
@@ -81,16 +81,17 @@ void wiggle() {
     lastSwitch = millis();
     firstWiggle = false;
   }
-    
+
   if(abs(millis() - lastSwitch) > switchTime) {
     lastSwitch = millis();
     state1 = !state1;
   }  
-  
+
   if(state1) {
-    moveWheels(20,-20);
-  } else {
-    moveWheels(-20,20);
+    moveWheels(interval,-interval);
+  } 
+  else {
+    moveWheels(-interval,interval);
   }
 }
 
@@ -101,13 +102,13 @@ void setup ()
   Serial.println("Robot 5");
   Serial.println("");
   AFMStop.begin(); // Start the top shield
-   
+
   stepper1.setMaxSpeed(1000.0);
   stepper1.setAcceleration(100.0);
-    
+
   stepper2.setMaxSpeed(1000.0);
   stepper2.setAcceleration(100.0);
-  
+
   stepper1.setSpeed(500);
   stepper2.setSpeed(500);
 }
@@ -119,21 +120,21 @@ void loop ()
     // read incoming serial data:
     char inChar = Serial.read();
     pt_loop(inChar);
+    //Serial.println(c);
     Serial.println("ok");
-   
+  } else {
+    pt_loop('q');
   }
 }
 
 /*
   PyroTech Loop - this is where you code goes. You will receive 
-  one character "c" at a time, each time a key stroke is received 
-  from the keyboard drivers station.  You can choose to have your 
-  code do whatever you want when that character arrives.
-*/
+ one character "c" at a time, each time a key stroke is received 
+ from the keyboard drivers station.  You can choose to have your 
+ code do whatever you want when that character arrives.
+ */
 void pt_loop(char c)
 {
-  Serial.println(c);
-
   switch (c) {
   case 'e':
     go_forward();
@@ -159,7 +160,11 @@ void pt_loop(char c)
   default:
     break;
   }
+  
+  stepper1.run();
+  stepper2.run();
 }
+
 
 
 
